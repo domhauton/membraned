d7031d74f42dd1da5b61c5bebf761804ff3f39c5
Dominic H <domhauton@gmail.com>
2017-03-17 22:19:15 +0000

    Soften rate-limiter tests
    Signed-off-by: Dominic H <domhauton@gmail.com>

--- src/test/java/com/domhauton/membrane/distributed/maintainance/RateLimiterTest.java
+++ src/test/java/com/domhauton/membrane/distributed/maintainance/RateLimiterTest.java
@@ -18,17 +18,17 @@ class RateLimiterTest {
   void testRateLimiting() throws Exception {
     Runnable runnableMock = Mockito.mock(Runnable.class);
 
-    RateLimiter uploadRateLimiter = new RateLimiter(runnableMock, Duration.millis(100));
+    RateLimiter uploadRateLimiter = new RateLimiter(runnableMock, Duration.millis(200));
 
     ScheduledExecutorService addExecutor = Executors.newSingleThreadScheduledExecutor();
     ScheduledFuture<?> looper = addExecutor.scheduleWithFixedDelay(uploadRateLimiter::schedule, 0, 10, TimeUnit.MILLISECONDS);
 
-    addExecutor.schedule(() -> Mockito.verify(runnableMock, Mockito.times(4)).run(), 420, TimeUnit.MILLISECONDS);
+    addExecutor.schedule(() -> Mockito.verify(runnableMock, Mockito.times(1)).run(), 300, TimeUnit.MILLISECONDS);
     addExecutor.schedule(() -> {
       looper.cancel(true);
-      Mockito.verify(runnableMock, Mockito.atMost(5)).run();
-    }, 570, TimeUnit.MILLISECONDS);
-    ScheduledFuture<?> testEnd = addExecutor.schedule(() -> Mockito.verify(runnableMock, Mockito.times(6)).run(), 790, TimeUnit.MILLISECONDS);
+      Mockito.verify(runnableMock, Mockito.atMost(2)).run();
+    }, 500, TimeUnit.MILLISECONDS);
+    ScheduledFuture<?> testEnd = addExecutor.schedule(() -> Mockito.verify(runnableMock, Mockito.times(3)).run(), 850, TimeUnit.MILLISECONDS);
 
     testEnd.get(1, TimeUnit.SECONDS);
   }
