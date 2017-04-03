b56be4c5bae0cbcd0852eca7a2fc736d1620ee4c
domhauton <domhauton@gmail.com>
2017-04-03 21:36:30 +0100

    Created Peer Cert Manager
    Signed-off-by: domhauton <domhauton@gmail.com>

---
+++ src/main/java/com/domhauton/membrane/network/auth/PeerCertManager.java
@@ -0,0 +1,126 @@
+package com.domhauton.membrane.network.auth;
+
+import com.domhauton.membrane.distributed.ContractManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * Created by dominic on 03/04/17.
+ */
+public class PeerCertManager {
+  private final Logger logger = LogManager.getLogger();
+  private final static String EXTENSION = ".crt";
+
+  private final Path certStoreFolder;
+  private final HashMap<String, X509Certificate> certificateMap;
+
+  private ContractManager contractManager;
+
+  public PeerCertManager(Path certStoreFolder) {
+    this.certStoreFolder = certStoreFolder;
+    this.certificateMap = loadCerts(certStoreFolder);
+  }
+
+  /**
+   * Persist peer certificate. Ensure peer is contracted before addition.
+   *
+   * @param userId          ID of peer
+   * @param x509Certificate Certificate to persist
+   */
+  public void addCertificate(String userId, X509Certificate x509Certificate) {
+    certificateMap.put(userId, x509Certificate);
+    Path certPath = getCertPath(userId);
+    try {
+      Files.deleteIfExists(certPath);
+      AuthFileUtils.writeCertificate(certPath, x509Certificate);
+    } catch (IOException e) {
+      logger.warn("Failed to write certificate to file. [{}]", certPath);
+    }
+
+    // Only sweep certs if there is an active contract manager!
+
+    getActiveContacts().ifPresent(this::removeNonContractedPeers);
+  }
+
+  private Path getCertPath(String userId) {
+    return Paths.get(certStoreFolder.toString() + File.separator + userId + EXTENSION);
+  }
+
+  private HashMap<String, X509Certificate> loadCerts(Path certStoreFolder) {
+    HashMap<String, X509Certificate> existingPeerCerts = new HashMap<>();
+    try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(certStoreFolder)) {
+      for (Path path : directoryStream) {
+        if (path.endsWith(EXTENSION)) {
+          try {
+            X509Certificate x509Certificate = AuthFileUtils.loadCertificate(path);
+            String peerName = path.getFileName().toString().replace(EXTENSION, "");
+            existingPeerCerts.put(peerName, x509Certificate);
+          } catch (AuthException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    } catch (IOException ex) {
+      logger.warn("IOException while loading existing certs.", ex.getMessage());
+    }
+    return existingPeerCerts;
+  }
+
+  private void removeNonContractedPeers(Set<String> contractedPeers) {
+    logger.info("Removing certificates for uncontracted peers.");
+    Set<String> redundantPeerCerts = certificateMap.keySet();
+    redundantPeerCerts.removeAll(contractedPeers);
+    redundantPeerCerts.stream()
+        .peek(certificateMap::remove)
+        .map(this::getCertPath)
+        .forEach(certPath -> {
+          try {
+            Files.deleteIfExists(certPath);
+          } catch (IOException e) {
+            logger.error("Failed to remove cert. [{}]", certPath);
+          }
+        });
+    logger.info("Certificate Removal Complete. Removed {} certs.", redundantPeerCerts.size());
+  }
+
+  private Optional<Set<String>> getActiveContacts() {
+    return contractManager == null ? Optional.empty() : Optional.of(contractManager.getContractedPeers());
+  }
+
+  /**
+   * Set the contract manager used to determine active contract list.
+   *
+   * @param contractManager the new contract manager
+   */
+  public void setContractManager(ContractManager contractManager) {
+    this.contractManager = contractManager;
+  }
+
+  /**
+   * Retrieves the certificate for the given peer.
+   *
+   * @param peerId UID for the peer
+   * @return Certificate request
+   * @throws NoSuchElementException If certificate does not exist
+   */
+  public X509Certificate getCertificate(String peerId) throws NoSuchElementException {
+    X509Certificate x509Certificate = certificateMap.get(peerId);
+    if (x509Certificate != null) {
+      return x509Certificate;
+    } else {
+      throw new NoSuchElementException("Certificate for " + peerId + " not stored.");
+    }
+  }
+}
