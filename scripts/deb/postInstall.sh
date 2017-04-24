62ee5c8a004f5f37977e96b89425307d0de07b0d
domhauton <domhauton@gmail.com>
2017-04-24 19:21:13 +0100

    Updated Packaging Scripts for GUI
    Signed-off-by: domhauton <domhauton@gmail.com>

--- scripts/deb/postInstall.sh
+++ scripts/deb/postInstall.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+tar -C /usr/share/membrane/lib -xzf /usr/share/membrane/lib/membrane-gui-v1.0.0-alpha.5.tar.gz
+
+rm /usr/share/membrane/lib/membrane-gui-v1.0.0-alpha.5.tar.gz
 cp /usr/share/membrane/bin/membraned.service /etc/systemd/system/membraned.service
 cp /usr/share/membrane/bin/autocomplete /etc/bash_completion.d/membrane
 
